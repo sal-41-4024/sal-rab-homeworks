4398264a91c5012fbc60212548ca9591984cf263
User <test@email.com>
2023-02-21 20:53:37 +0300

    Task Final is Done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,19 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {client: "", order: {}, goods: []};
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let item of goods) {
+        data.goods.push({title: item.title, count: item.count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.client = `${name} ${phone}`;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`
+    data.order.sum = sum;
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
+    const obj = {data: data};
+    let jsonData = JSON.stringify(obj);
+    
     return jsonData;
 }
