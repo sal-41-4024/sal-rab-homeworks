746dfdd2d0f976e49d768dd9d229d664f74f4f8c
User <test@email.com>
2023-02-22 17:34:55 +0300

    Tasks updates

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,18 +6,21 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
+
     let shippingSum;
 
     // если productSum равно 0,
     // то shippingSum присвоить значение 0
     // если productSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
+
     if ((productSum == 0) || (productSum >= freeShippingMinSum)) {
         shippingSum = 0;
     }
 
     // если productSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+
     if ((productSum > 0) && (productSum < freeShippingMinSum)) {
         shippingSum = shippingPrice;
     }
@@ -40,6 +43,7 @@ function calcDiscount(sum, min, discount) {
     // если productSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productSum,
     // иначе присвойте discountSum значение 0
+
     if (productSum >= discountMinSum) {
         discountSum = (discountPart * productSum) / 100;
     }
@@ -59,24 +63,30 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
     // создайте переменную totalSum
+
     let totalSum;
 
     // присвойте totalSum значение productSum
+
     totalSum = productSum;
 
     // уменьшите totalSum на discountSum
+
     totalSum -= discountSum;
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
+
     totalSum += shippingSum;
 
     // создайте переменную freeShipping
+
     let freeShipping;
 
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    
     freeShipping = (shippingSum == 0);
 
     // Конец решения задачи №2.3.
